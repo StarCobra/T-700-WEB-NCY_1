b261db14f64e0cbfaf0db535f081aac352fc8513
Arnaud Bourgoin <arnaud.bourgoin@outlook.com>
2023-11-14 14:48:06 +0100

    first commit

--- api/index.js
+++ api/index.js
@@ -4,8 +4,20 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send(test());
+});
+
+app.post("/users/register", (req, res) => {
+  try {
+    let body = req.body.test;
+    console.log(body);
+    res.send("Data Received :" + JSON.stringify(body));
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
