c464f5d8a8d1c5bfb4b1a4057f64cfbde6e43e62
StarCobra <damienpoirot@yahoo.com>
2023-11-14 14:14:09 +0100

    fix: Update docker for dev

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
-const express = require("express");
+import { test } from "./test.js";
+import express from "express";
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(test());
 });
 
 app.listen(port, () => {
