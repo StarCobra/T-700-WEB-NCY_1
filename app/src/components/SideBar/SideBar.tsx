349facbf5f66a7131479ba41fa21b3da4a8826fd
KilianSoudier <122787221+KilianSoudier@users.noreply.github.com>
2023-11-16 23:20:32 +0100

    [FIX] scss files cleaning

--- app/src/components/SideBar/SideBar.tsx
+++ app/src/components/SideBar/SideBar.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import LogoSideBar from './Logo';
 import UserLabel from './UserLabel';
+import '../../style/default.scss'
+import '../../style/sidebar.scss'
 import Menus from './Menus';
 
 
