c3ebd22a0207252c16e8b817514a63b4b104a602
KilianSoudier <122787221+KilianSoudier@users.noreply.github.com>
2023-11-21 14:40:26 +0100

    [FEAT] add crypto array and add animations to border

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import SideBar from './components/SideBar/SideBar';
+import CryptoChart from './components/CryptoChart/CryptoChart';
+import CryptoArrayDisplay from './components/CryptoArray/CryptoArrayDisplay';
 function App() {
   return (
     <div className="layoutContainer">
       <SideBar />
-      <div className="dataContainer"></div>
+      <div className="displayContainer">
+        <div className="dataContainer">
+          <CryptoChart />
+        </div>
+        <div className="dataContainer">
+          <CryptoArrayDisplay />
+        </div>
+      </div>
     </div>
 
   );
