b886322280a90bf0fbf6fbb6d5cc265e8c58f9b8
KilianSoudier <122787221+KilianSoudier@users.noreply.github.com>
2023-11-27 11:04:19 +0100

    [FEAT] Add filter bar to keywords

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SideBar from './components/SideBar/SideBar';
 import MultipleArticlesDisplay from './components/Articles/MultipleArticlesDisplay';
+import KeywordFilter from './components/Articles/KeywordFilter';
 function App() {
   return (
     <div className="layoutContainer">
       <SideBar />
       <div className="displayContainer">
         <div className="dataContainer">
+          <KeywordFilter />
           <MultipleArticlesDisplay />
         </div>
         {/* <div className="dataContainer">
