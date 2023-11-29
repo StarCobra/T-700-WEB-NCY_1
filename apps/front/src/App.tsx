e8191165a1db7bc2cdf925d378bb995d549a9f86
KilianSoudier <122787221+KilianSoudier@users.noreply.github.com>
2023-11-29 21:12:06 +0100

    [FEAT] Add custom style to all buttons and profile page

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import SideBar from './components/SideBar/SideBar';
-import MultipleArticlesDisplay from './components/Articles/ArticleThumbnails/MultipleArticlesDisplay';
-import KeywordFilter from './components/Articles/ArticleThumbnails/KeywordFilter';
-import FullArticleDisplay from './components/Articles/FullArticle/FullArticleDisplay';
+import Profile from './components/Profile/Profile';
 function App() {
   return (
     <div className="layoutContainer">
       <SideBar />
       <div className="displayContainer">
         <div className="dataContainer">
-          <FullArticleDisplay />
+          <Profile />
         </div>
-        {/* <div className="dataContainer">
-          <CryptoArrayDisplay />
-        </div> */}
       </div>
     </div>
 
---
+++ apps/front/src/components/Profile/ProfileThumbnail.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import exampleImage from '../../assets/profile/profileExample.png'
+
+export default function ProfileThumbnail() {
+  return (
+    <div className="profileThumbnailContainer">
+        <img src={exampleImage} />
+    </div>
+  )
+}
