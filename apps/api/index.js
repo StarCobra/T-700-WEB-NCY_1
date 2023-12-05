e22aa53a6b99d5bda98e79a6235bda0041120f6f
Arnaud541 <arnaud.bourgoin@outlook.com>
2023-12-05 09:35:37 +0100

    feat: changement authentification avec JWT

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,25 +1,17 @@
 import express from "express";
-import session from "express-session";
 import passport from "passport";
 import userRoutes from "./user/user.js";
 import cryptoRoutes from "./crypto/crypto.js";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3005;
 
+dotenv.config();
+
 app.use(express.json());
-
-// Middleware pour la gestion des sessions
-app.use(
-    session({
-      secret: process.env.SESSION_SECRET_KEY,
-      resave: true,
-      saveUninitialized: true,
-    })
-);
-
 app.use(passport.initialize());
-app.use(passport.session());
+
 app.use("/users", userRoutes);
 app.use("/cryptos", cryptoRoutes);
 
--- apps/api/user/user.js
+++ apps/api/user/user.js
@@ -1,10 +1,42 @@
 import express from "express";
 import passport from "passport";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import "./auth-basic.js";
-import "./auth-google.js";
 import { createDatabase } from "../database/create.js";
+
 const router = express.Router();
+
+function verifyToken(req, res, next) {
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Token non fourni" });
+  }
+
+  jwt.verify(
+    token,
+    process.env.SECRET_KEY_JWT,
+    { algorithms: "HS256" },
+    (err, user) => {
+      if (err) {
+        if (err.name === "TokenExpiredError") {
+          // const refreshToken = jwt.sign(req.user, process.env.SECRET_KEY, {
+          //   expiresIn: "1h",
+          // });
+          return res.status(401).json({ message: "Token expiré" });
+        }
+        return res
+          .status(403)
+          .json({ error: err, message: "Token non valide" });
+      }
+
+      req.user = user;
+      next();
+    }
+  );
+}
 
 router.post("/register", async (req, res) => {
   try {
@@ -20,7 +52,7 @@ router.post("/register", async (req, res) => {
     const pool = await createDatabase();
     const connection = await pool.getConnection();
     await connection.query(
-      "INSERT INTO users (name, password, birth_date) VALUES (?, ?, ?)",
+      "INSERT INTO user (name, password, birth_date) VALUES (?, ?, ?)",
       [user.username, hash, user.birth_date]
     );
 
@@ -34,12 +66,15 @@ router.post("/register", async (req, res) => {
 router.post(
   "/login",
   passport.authenticate("local", {
-    failureRedirect: "/login-failed",
+    // failureRedirect: "/login-failed",
+    session: false,
   }),
   (req, res) => {
-    res
-      .status(200)
-      .json({ message: "Successful authentication", redirectUrl: "/" });
+    res.status(200).json({
+      message: "Successful authentication",
+      redirectUrl: "/",
+      token: `Bearer ${req.user}`,
+    });
   }
 );
 
@@ -59,101 +94,99 @@ router.get(
   "/auth/google/callback",
   passport.authenticate("google", { failureRedirect: "/login-failed" }),
   function (req, res) {
-    res.redirect("/");
+    res.status(200).send({ data: req.user });
+    // res.redirect("/");
   }
 );
 
-router.post("/logout", (req, res) => {
-  // Vérifiez si l'utilisateur est connecté avant de le déconnecter
-  if (req.isAuthenticated()) {
-    // Utilisez la fonction de Passport pour déconnecter l'utilisateur
-    req.logout();
-    // Renvoyez une réponse indiquant que la déconnexion est réussie
-    res.status(200).json({ message: "Logout successful", redirectUrl: "/" });
-  } else {
-    // Renvoyez une réponse indiquant que l'utilisateur n'est pas connecté
-    res.status(401).json({ message: "User not authenticated" });
-  }
-});
+// router.post("/logout", (req, res) => {
+//   // Vérifiez si l'utilisateur est connecté avant de le déconnecter
+//   if (req.isAuthenticated()) {
+//     // Utilisez la fonction de Passport pour déconnecter l'utilisateur
+//     req.logout();
+//     // Renvoyez une réponse indiquant que la déconnexion est réussie
+//     res.status(200).json({ message: "Logout successful", redirectUrl: "/" });
+//   } else {
+//     // Renvoyez une réponse indiquant que l'utilisateur n'est pas connecté
+//     res.status(401).json({ message: "User not authenticated" });
+//   }
+// });
 
 // Route pour gérer l'échec d'authentification côté serveur
 router.get("/login-failed", (req, res) => {
   res.redirect("/users/login?error=auth_failed");
 });
 
-router.get("/profile", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.status(200).json({ user: req.user });
-  } else {
-    res.status(401).json({ message: "Unauthorized" });
-  }
+router.get("/profile", verifyToken, (req, res) => {
+  console.log(req.user);
+  res.status(200).json({ user: req.user });
 });
 
-router.put("/profile", function (req, res) {
-  if (req.isAuthenticated()) {
-    let user_edit_informations = req.body; // list cryptos, list keywords, u
+// router.put("/profile", function (req, res) {
+//   if (req.isAuthenticated()) {
+//     let user_edit_informations = req.body; // list cryptos, list keywords
 
-    if (user_edit_informations.list_crypto.is_change) {
-      user_edit_informations.list_crypto.forEach((crypto) => {
-        if (crypto.is_add) {
-          const query = `INSERT INTO chosen_crypto (crypto_id, user_id) VALUES (?,?);`;
-          db.query(query, [crypto.id, req.user.id], (err, insertResult) => {
-            if (err) {
-              res.status(500).send({ message: "Error database" });
-            }
-          });
-        }
-        if (crypto.is_delete) {
-          const query = `DELETE FROM chosen_crypto WHERE crypto_id = ? AND user_id = ?`;
-          db.query(query, [crypto.id, req.user.id], (err, insertResult) => {
-            if (err) {
-              res.status(500).send({ message: "Error database" });
-            }
-          });
-        }
-      });
-    } else {
-      if (user_edit_informations.list_keywords.is_change) {
-        user_edit_informations.list_keywords.forEach((keyword) => {
-          if (keyword.is_add) {
-            const query = `INSERT INTO favorite_keywords (keyword_id, user_id) VALUES (?,?);`;
-            db.query(query, [keyword.id, req.user.id], (err, insertResult) => {
-              if (err) {
-                res.status(500).send({ message: "Error database" });
-              }
-            });
-          }
+//     if (user_edit_informations.list_crypto.is_change) {
+//       user_edit_informations.list_crypto.forEach((crypto) => {
+//         if (crypto.is_add) {
+//           const query = `INSERT INTO chosen_crypto (crypto_id, user_id) VALUES (?,?);`;
+//           db.query(query, [crypto.id, req.user.id], (err, insertResult) => {
+//             if (err) {
+//               res.status(500).send({ message: "Error database" });
+//             }
+//           });
+//         }
+//         if (crypto.is_delete) {
+//           const query = `DELETE FROM chosen_crypto WHERE crypto_id = ? AND user_id = ?`;
+//           db.query(query, [crypto.id, req.user.id], (err, insertResult) => {
+//             if (err) {
+//               res.status(500).send({ message: "Error database" });
+//             }
+//           });
+//         }
+//       });
+//     } else {
+//       if (user_edit_informations.list_keywords.is_change) {
+//         user_edit_informations.list_keywords.forEach((keyword) => {
+//           if (keyword.is_add) {
+//             const query = `INSERT INTO favorite_keywords (keyword_id, user_id) VALUES (?,?);`;
+//             db.query(query, [keyword.id, req.user.id], (err, insertResult) => {
+//               if (err) {
+//                 res.status(500).send({ message: "Error database" });
+//               }
+//             });
+//           }
 
-          if (keyword.is_delete) {
-            const query = `DELETE FROM favorite_keywords WHERE keyword_id = ? AND user_id = ?`;
-            db.query(query, [keyword.id, req.user.id], (err, insertResult) => {
-              if (err) {
-                res.status(500).send({ message: "Error database" });
-              }
-            });
-          }
-        });
-      } else {
-        if (user_edit_informations.user.is_change) {
-          const user = user_edit_informations.user;
-          const query = `UPDATE users SET username = ?, user_email = ? WHERE user_id = ?`;
-          db.query(
-            query,
-            [user.username, user.user_email, req.user.id],
-            (err, insertResult) => {
-              if (err) {
-                res.status(500).send({ message: "Error database" });
-              }
-            }
-          );
-        }
-      }
-    }
+//           if (keyword.is_delete) {
+//             const query = `DELETE FROM favorite_keywords WHERE keyword_id = ? AND user_id = ?`;
+//             db.query(query, [keyword.id, req.user.id], (err, insertResult) => {
+//               if (err) {
+//                 res.status(500).send({ message: "Error database" });
+//               }
+//             });
+//           }
+//         });
+//       } else {
+//         if (user_edit_informations.user.is_change) {
+//           const user = user_edit_informations.user;
+//           const query = `UPDATE users SET username = ?, user_email = ? WHERE user_id = ?`;
+//           db.query(
+//             query,
+//             [user.username, user.user_email, req.user.id],
+//             (err, insertResult) => {
+//               if (err) {
+//                 res.status(500).send({ message: "Error database" });
+//               }
+//             }
+//           );
+//         }
+//       }
+//     }
 
-    res.status(200).json();
-  } else {
-    res.status(401).json({ message: "Unauthorized" });
-  }
-});
+//     res.status(200).json();
+//   } else {
+//     res.status(401).json({ message: "Unauthorized" });
+//   }
+// });
 
 export default router;
