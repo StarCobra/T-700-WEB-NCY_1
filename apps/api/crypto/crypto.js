dedbc64d185fd07501e1d0d1ae917fb90919fbcc
Arnaud541 <arnaud.bourgoin@outlook.com>
2023-12-11 11:16:45 +0100

    feat: add delete crypto route

--- apps/api/crypto/crypto.js
+++ apps/api/crypto/crypto.js
@@ -19,6 +19,38 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.delete("/:cmid", verifyToken, isAdmin, async(req, res) => {
+  // cmid: cryptocurrency Id. User MUST be logged in as well as the ADMINISTRATOR. Deletes
+  // a cryptocurrency (meaning that your platform does not know this currency anymore.)
+  try {
+    const cmid = req.params.cmid;
+
+    if(cmid) {
+      const pool = await createDatabase();
+      const connection = await pool.getConnection();
+      await connection.query("DELETE FROM crypto WHERE id = ?", [cmid]);
+
+      res.status(201).send("The crypto has been deleted");
+    }
+    else {
+      res.status(400).send("Error parameters")
+    }
+  } catch (error) {
+    res.status(500).send({ message: error + "Internal server error" });
+  }
+})
+
+router.get("/:cmid/history/:period", verifyToken, async (req,res) => {
+  // cmid: cryptocurrency Id. period: daily, hourly or minute. User MUST be logged in. Provides
+  // the price history of a cryptocurrency. For each period:
+  // – opening, highest, lowest and closing exchange rates
+  // Depending on the periods, the history is shorter or longer:
+  // – daily: Last 60 days, so 60 periods a day ;
+  // – hourly: 48 last hours, so 48 periods of one hour ;
+  // – minute: last 2 hours, so 60 periods of one minute.
+
+})
+
 router.post("/", verifyToken, isAdmin, async (req, res) => {
   // User MUST be logged in as well as the ADMINISTRATOR. Add a cryptocurrency to your plat-form.
   // A form must be attached to the request and contain at least the cryptocurrency code,
