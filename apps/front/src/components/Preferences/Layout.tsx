903add358a9a8cd171b43468d2b2d658eebe4a33
StarCobra <damienpoirot@yahoo.com>
2023-12-12 15:36:28 +0100

    feat(front): add preferences page

---
+++ apps/front/src/components/Preferences/Layout.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { Box } from "@mui/material";
+import CryptoArray from "../Crypto/Array";
+import Select from "../Select";
+
+export default function Layout() {
+  const [favoriteCrypto, setFavoriteCrypto] = React.useState([]);
+
+  // TODO : remplacer par valeur de l'API
+  const options = [
+    { value: "BTC", label: "BTC" },
+    { value: "ETH", label: "ETH" },
+    { value: "SOL", label: "SOL" },
+    { value: "ADA", label: "ADA" },
+    { value: "DOT", label: "DOT" },
+    { value: "LUNA", label: "LUNA" },
+    { value: "LINK", label: "LINK" },
+  ];
+
+  // TODO : remplacer par valeur de l'API
+  const resource = [
+    {
+      id: "BTC",
+      current_price: 50000,
+      percent_price_day: 0.5,
+      highest_price_day: 55000,
+      lowest_price_day: 45000,
+    },
+    {
+      id: "ETH",
+      current_price: 5000,
+      percent_price_day: 0.5,
+      highest_price_day: 5500,
+      lowest_price_day: 4500,
+    },
+    {
+      id: "SOL",
+      current_price: 500,
+      percent_price_day: 0.5,
+      highest_price_day: 550,
+      lowest_price_day: 450,
+    },
+    {
+      id: "ADA",
+      current_price: 5,
+      percent_price_day: 0.5,
+      highest_price_day: 5.5,
+      lowest_price_day: 4.5,
+    },
+    {
+      id: "DOT",
+      current_price: 50,
+      percent_price_day: 0.5,
+      highest_price_day: 55,
+      lowest_price_day: 45,
+    },
+    {
+      id: "LUNA",
+      current_price: 50,
+      percent_price_day: 0.5,
+      highest_price_day: 55,
+      lowest_price_day: 45,
+    },
+    {
+      id: "LINK",
+      current_price: 50,
+      percent_price_day: 0.5,
+      highest_price_day: 55,
+      lowest_price_day: 45,
+    },
+  ];
+
+  return (
+    <Box className="preferencesLayout">
+      <Box>
+        <h3>Preferences page</h3>
+        <p>
+          This page is used to manage your preferences. You can choose your
+          favorite crypto.
+        </p>
+        <p>
+          The chosen crypto will be displayed on the home page and you&apos;ll
+          receive in priority the new articles about it.
+        </p>
+      </Box>
+      <Box>
+        <h3>Table of cryptos available</h3>
+        <CryptoArray resource={resource} />
+      </Box>
+      <Box>
+        <Box>
+          <h3>Add crypto to your favorite cypto</h3>
+          <Select
+            name="Select"
+            label="Favorite crypto(s) chosen:"
+            type="multiple"
+            options={options}
+            handleChange={(e: any) => setFavoriteCrypto(e.target.value)}
+          />
+        </Box>
+        <h3>
+          You have chosen {favoriteCrypto.length} crypto(s) as favorite crypto :
+        </h3>
+        {favoriteCrypto.length > 0 && (
+          <ul>
+            {favoriteCrypto.map((crypto: any) => (
+              <li key={crypto}>{crypto}</li>
+            ))}
+          </ul>
+        )}
+      </Box>
+    </Box>
+  );
+}
