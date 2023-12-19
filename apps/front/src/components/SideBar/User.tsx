b5d140c2c06aefd808057d4fe6d2bcede6e07d37
StarCobra <damienpoirot@yahoo.com>
2023-12-19 16:58:55 +0100

    feat(front): update login and sign in

--- apps/front/src/components/SideBar/User.tsx
+++ apps/front/src/components/SideBar/User.tsx
@@ -2,20 +2,33 @@ import React from "react";
 import profilePic from "../../assets/sideBar/profil.png";
 import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 
 export default function User(props: any) {
   const { user } = props;
 
   return (
-    <Link to="/profile" className="userLocation">
-      <Box className="userProfilePicture">
-        <img src={profilePic} alt="" />
-      </Box>
+    <Link to={user ? "/profile" : "/login"} className="userLocation">
+      {user ? (
+        <>
+          <Box className="userProfilePicture">
+            <img src={profilePic} alt="" />
+          </Box>
 
-      <Box className="usernameContainer">
-        <Box className="userName">{user?.name}</Box>
-        <Box className="userMail">{user?.mail}</Box>
-      </Box>
+          <Box className="usernameContainer">
+            <Box className="userName">{user?.name}</Box>
+            <Box className="userMail">{user?.mail}</Box>;
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box className="menuIcon">
+            <HomeOutlinedIcon />
+          </Box>
+
+          <Box className="menuLabel">Log in</Box>
+        </>
+      )}
     </Link>
   );
 }
