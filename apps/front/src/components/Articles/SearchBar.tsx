4ff378a22fbe4f4046545e066d551c95400c8691
KilianSoudier <122787221+KilianSoudier@users.noreply.github.com>
2023-12-21 03:27:19 +0100

    feat(front, css) rebasing dev

--- apps/front/src/components/Articles/SearchBar.tsx
+++ apps/front/src/components/Articles/SearchBar.tsx
@@ -4,29 +4,29 @@ import { Box } from "@mui/material";
 import Select from "../Select";
 
 export default function SearchBar() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [keywords, setKeywords] = useState([]);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const [keywords, setKeyword] = useState([]);
 
-  // TODO : remplacer par valeur de l'API
-  const options = [
+ // TODO : remplacer par valeur de l'API
+ const options = [
     { value: 1, label: "Income" },
     { value: 2, label: "BTC" },
-    { value: 3, label: "Bankrupt" },
+    { value: 3, label: "Bankerupt" },
     { value: 4, label: "ETH" },
   ];
 
   return (
     <form className="searchBarContainer">
-      <Box className="selectSearchContainer">
-        <Box className="selectSearch">
-          <Select
-            name="Select"
-            label="Choose a keyword"
-            options={options}
-            handleChange={(e: any) => setKeywords(e.target.value)}
-          />
+        <Box className="selectSearchContainer">
+            <Box className="selectSearch">
+            <Select
+                name="Select"
+                label="Choose a keyword"
+                options={options}
+                handleChange={(e: any) => setKeyword(e.target.value)}
+            />
+            </Box>
         </Box>
-      </Box>
     </form>
   );
 }
